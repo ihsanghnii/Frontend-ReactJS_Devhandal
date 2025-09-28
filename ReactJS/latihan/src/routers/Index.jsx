59480f1415fc5d6806683cc21743359c368c02a0
ihsanghnii <ihsanghan01@gmail.com>
2025-09-28 07:58:51 +0700

    update

--- ReactJS/latihan/src/routers/Index.jsx
+++ ReactJS/latihan/src/routers/Index.jsx
@@ -6,12 +6,14 @@ import Blog from "../pages/blogs/Index";
 import Post from "../pages/blogs/_id"
 import About from "../pages/About";
 
+import ErrorPage from "../components/ErrorPage";
 import { posts, postById } from "../apis/loaders";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout/ >,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
